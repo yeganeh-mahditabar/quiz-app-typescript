eda1b341d9e6e1842946792b7346eeb2169e6b0f
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2025-04-28 03:17:40 +0430

    next question

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react"
 import { questions } from "./questions"
 
 
 function Quiz() {
+
+  const [score, setScore] = useState<number>(0)
+  const [selectedOption, setSelectedOption] = useState<string>("")
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0)
+
+
+  function handleAnswer(option:string){
+    setSelectedOption(option)
+    if(option == questions[currentQuestion].answer){
+        setScore(score+1)
+    }
+
+  }    
   return (
     <div className="quiz-container">
         <div className="question-box">
-            <h3>{questions[0].question}</h3>
+            <h3>{questions[currentQuestion].question}</h3>
             <ul>
                 {
-                    questions[0].options.map(
-                        (option:string)=> <li>{option}</li>
+                    questions[currentQuestion].options.map(
+                        (option:string)=> (
+                            <li onClick={()=> handleAnswer(option)}
+                            
+                            >{option}</li>
+                        )
                     )
                 }
             </ul>
